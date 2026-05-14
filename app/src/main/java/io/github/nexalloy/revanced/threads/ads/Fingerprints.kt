ae1d27ab31552f9fbe5e3b38f53adfd78466c970
gnadgnaoh <117189041+gnadgnaoh@users.noreply.github.com>
2026-05-14 14:45:49 +0700

    Update Fingerprints.kt

--- app/src/main/java/io/github/nexalloy/revanced/threads/ads/Fingerprints.kt
+++ app/src/main/java/io/github/nexalloy/revanced/threads/ads/Fingerprints.kt
@@ -1,4 +1,4 @@
-package io.github.nexalloy.revanced.threads.ads
+package io.github.nexalloy.revanced.instagram.ads
 
 import io.github.nexalloy.morphe.findMethodDirect
 
@@ -16,6 +16,34 @@ val adSponsoredContentFingerprint = findMethodDirect {
         matcher {
             returnType = "boolean"
             usingStrings("SponsoredContentController.insertItem")
+        }
+    }.single()
+}
+
+val adV2ControllerClassFingerprint = findMethodDirect {
+    findMethod {
+        matcher {
+            usingStrings("SponsoredContentControllerV2.onCurrentIndexChanged")
+        }
+    }.first()
+}
+
+val adV2DeliveryFingerprint = findMethodDirect {
+    val v2ClassName = adV2ControllerClassFingerprint().declaredClassName
+    findMethod {
+        matcher {
+            declaredClass(v2ClassName)
+            usingStrings("onSponsoredContentDelivered")
+            paramCount = 7
+        }
+    }.first()
+}
+
+val adV2InsertGateFingerprint = findMethodDirect {
+    findMethod {
+        matcher {
+            returnType = "boolean"
+            usingStrings("injection_orchestrator_position_passed_with_insertion_but_not_impression_")
         }
     }.single()
 }
