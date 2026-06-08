cf1c85df5fbbe605485294ce1192e0f080848267
gnadgnaoh <117189041+gnadgnaoh@users.noreply.github.com>
2026-06-08 15:43:19 +0700

    Update HideAdsPatch.kt

--- app/src/main/java/io/github/nexalloy/revanced/threads/ads/HideAdsPatch.kt
+++ app/src/main/java/io/github/nexalloy/revanced/threads/ads/HideAdsPatch.kt
@@ -5,9 +5,15 @@ import io.github.nexalloy.patch
 
 val HideAds = patch(
     name = "Hide ads",
-    description = "Hides injected ads and sponsored content in the feed." // Thêm description cho chuẩn form nhé
+    description = "Hides injected ads, sponsored content, and paid partnership posts in feed."
 ) {
-    ::adInjectorFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
 
-    ::adSponsoredContentFingerprint.hookMethod(XC_MethodReplacement.returnConstant(false))
-}
+    runCatching {
+        ::adInjectorFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
+        ::adSponsoredContentFingerprint.hookMethod(XC_MethodReplacement.returnConstant(false))
+        ::adInsertionActionFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
+        ::adContentDeliveredFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
+        ::spoolAdInjectorFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
+        ::paidPartnershipLabelFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
+        ::adMetadataFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
+    }
