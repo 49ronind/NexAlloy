1bdad18195516e9509d1e64c270c1789b7ffcea7
gnadgnaoh <117189041+gnadgnaoh@users.noreply.github.com>
2026-06-28 11:31:24 +0700

    Create TwitterHook.kt

---
+++ app/src/main/java/io/github/nexalloy/morphe/twitter/TwitterHook.kt
@@ -0,0 +1,37 @@
+package io.github.nexalloy.morphe.twitter
+
+import io.github.nexalloy.Patch
+import io.github.nexalloy.morphe.twitter.ads.timelineEntryHook.HideAds
+import io.github.nexalloy.morphe.twitter.ads.timelineEntryHook.HideRecommendationItems
+import io.github.nexalloy.morphe.twitter.featureFlag.DisableChirpFont
+import io.github.nexalloy.morphe.twitter.link.cleartrackingparams.ClearTrackingParams
+import io.github.nexalloy.morphe.twitter.link.unshorten.NoShortenedUrl
+import io.github.nexalloy.morphe.twitter.misc.blockRedirectToXLite.BlockRedirectingToXLite
+import io.github.nexalloy.morphe.twitter.timeline.disableAutoScroll.DisableAutoScroll
+import io.github.nexalloy.morphe.twitter.timeline.forceHD.ForceHD
+import io.github.nexalloy.morphe.twitter.timeline.removePremiumUpsell.RemovePremiumUpsell
+import io.github.nexalloy.morphe.twitter.timeline.sensitivemediasettings.ShowSensitiveMedia
+
+
+val TwitterPatches: Array<Patch> = arrayOf(
+    // Ads / recommendations
+    HideAds,
+    HideRecommendationItems,
+
+    // Feature-flag driven toggles
+    DisableChirpFont,
+
+    // Links
+    ClearTrackingParams,
+    NoShortenedUrl,
+
+    // Misc
+    BlockRedirectingToXLite,
+
+    // Timeline / tweet UI
+    DisableAutoScroll,
+    ForceHD,
+    RemovePremiumUpsell,
+    ShowSensitiveMedia,
+
+)
