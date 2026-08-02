19d1a126a6ac534b6d47586b45784978b1baf7f1
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-08-02 17:05:13 +0700

    Update DisableShortsResumingOnStartupPatch.kt

--- app/src/main/java/io/github/nexalloy/morphe/youtube/layout/shortsnoresume/DisableShortsResumingOnStartupPatch.kt
+++ app/src/main/java/io/github/nexalloy/morphe/youtube/layout/shortsnoresume/DisableShortsResumingOnStartupPatch.kt
@@ -1,9 +1,12 @@
 package io.github.nexalloy.morphe.youtube.layout.shortsnoresume
 
 import app.morphe.extension.youtube.patches.DisableShortsResumingOnStartupPatch
+import io.github.nexalloy.morphe.shared.misc.litho.filter.featureFlagCheck
 import io.github.nexalloy.morphe.shared.misc.settings.preference.SwitchPreference
 import io.github.nexalloy.morphe.youtube.misc.settings.PreferenceScreen
 import io.github.nexalloy.patch
+
+internal const val USER_WAS_IN_SHORTS_FEATURE_FLAG = 45358360L
 
 val DisableShortsResumingOnStartup = patch(
     name = "Disable Shorts resuming on startup",
@@ -17,10 +20,11 @@ val DisableShortsResumingOnStartup = patch(
     // TODO UserWasInShortsListenerFingerprint (20.03-21.02) — METHOD_MID
     // TODO UserWasInShortsLegacyFingerprint (<20.03) — METHOD_MID
 
-    UserWasInShortsConfigFingerprint.hookMethod {
-        before {
-            if (DisableShortsResumingOnStartupPatch.disableShortsResumingOnStartup()) {
-                it.result = false
+    ::featureFlagCheck.hookMethod {
+        after {
+            if (it.args[0] == USER_WAS_IN_SHORTS_FEATURE_FLAG) {
+                it.result = DisableShortsResumingOnStartupPatch
+                    .disableShortsResumingOnStartup(it.result as Boolean)
             }
         }
     }
