9493ddc6aab8f9e5143e74b59521dbde67931492
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-08-02 16:27:20 +0700

    Update Fingerprints.kt

--- app/src/main/java/io/github/nexalloy/morphe/music/ad/Fingerprints.kt
+++ app/src/main/java/io/github/nexalloy/morphe/music/ad/Fingerprints.kt
@@ -22,19 +22,20 @@ val showVideoAds = findMethodDirect {
 
 val hideGetPremiumFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    returns("V")
+    parameters()
     strings("FEmusic_history", "FEmusic_offline")
 }
 
 internal val membershipSettingsFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
     returns("Ljava/lang/CharSequence;")
+    parameters()
     opcodes(
         Opcode.IGET_OBJECT,
         Opcode.INVOKE_INTERFACE,
         Opcode.MOVE_RESULT_OBJECT,
         Opcode.INVOKE_VIRTUAL,
         Opcode.MOVE_RESULT_OBJECT,
-        Opcode.IF_EQZ,
-        Opcode.IGET_OBJECT,
     )
 }
