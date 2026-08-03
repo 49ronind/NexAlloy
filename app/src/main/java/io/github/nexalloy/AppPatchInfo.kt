37587f2edc0a54d543131b111eefc603a4327982
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-08-03 21:32:58 +0700

    Update AppPatchInfo.kt

--- app/src/main/java/io/github/nexalloy/AppPatchInfo.kt
+++ app/src/main/java/io/github/nexalloy/AppPatchInfo.kt
@@ -1,13 +1,18 @@
 package io.github.nexalloy
 
 import io.github.nexalloy.hoodles.morphe.alltrails.AllTrailsPatches
+import io.github.nexalloy.morphe.google.GoogleDiscoverPatches
 import io.github.nexalloy.morphe.music.YTMusicPatches
 import io.github.nexalloy.morphe.reddit.RedditPatches
 import io.github.nexalloy.morphe.youtube.YouTubePatches
 import io.github.nexalloy.revanced.googlephotos.GooglePhotosPatches
-import io.github.nexalloy.revanced.meta.MetaPatches
+import io.github.nexalloy.revanced.instagram.InstagramPatches
+import io.github.nexalloy.revanced.threads.ThreadsPatches
+import io.github.nexalloy.revanced.facebook.FacebookPatches
 import io.github.nexalloy.revanced.photomath.PhotomathPatches
 import io.github.nexalloy.revanced.strava.StravaPatches
+import io.github.nexalloy.morphe.twitter.TwitterPatches
+import io.github.nexalloy.morphe.twitter.utils.Constants.PACKAGE_NAME as TWITTER_PACKAGE_NAME
 
 class AppPatchInfo(val appName: String, val packageName: String, val patches: Array<Patch>)
 
@@ -17,10 +22,13 @@ val appPatchConfigurations = listOf(
     AppPatchInfo("Reddit", "com.reddit.frontpage", RedditPatches),
     AppPatchInfo("Google Photos", "com.google.android.apps.photos", GooglePhotosPatches),
     AppPatchInfo("Photomath", "com.microblink.photomath", PhotomathPatches),
-    AppPatchInfo("Instagram", "com.instagram.android", MetaPatches),
-    AppPatchInfo("Threads", "com.instagram.barcelona", MetaPatches),
+    AppPatchInfo("Instagram", "com.instagram.android", InstagramPatches),
+    AppPatchInfo("Threads", "com.instagram.barcelona", ThreadsPatches),
     AppPatchInfo("Strava", "com.strava", StravaPatches),
     AppPatchInfo("AllTrails", "com.alltrails.alltrails", AllTrailsPatches),
+    AppPatchInfo("Facebook", "com.facebook.katana", FacebookPatches),
+    AppPatchInfo("Google (Discover)", "com.google.android.googlequicksearchbox", GoogleDiscoverPatches),
+    AppPatchInfo("Twitter/X", TWITTER_PACKAGE_NAME, TwitterPatches),
 )
 
 val patchesByPackage = appPatchConfigurations.associate { it.packageName to it.patches }
