932d6cca759f927af004921a583ab4c94654f280
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2026-09-16 14:18:36 +0800

    YouTube: Remove `Report AI channel` entry

--- app/src/main/java/io/github/nexalloy/morphe/youtube/layout/hide/general/HideLayoutComponentsPatch.kt
+++ app/src/main/java/io/github/nexalloy/morphe/youtube/layout/hide/general/HideLayoutComponentsPatch.kt
@@ -259,17 +259,17 @@ val HideLayoutComponents = patch(
                         )
                     )
                 ),
-                PreferenceCategory(
-                    key = "morphe_aislist_submit_category",
-                    sorting = Sorting.UNSORTED,
-                    preferences = setOf(
-                        SwitchPreference("morphe_aislist_submit_flyout_menu", summary = true),
-                        TextPreference(
-                            key = "morphe_aislist_submit_username",
-                            inputType = InputType.TEXT
-                        )
-                    )
-                ),
+//                PreferenceCategory(
+//                    key = "morphe_aislist_submit_category",
+//                    sorting = Sorting.UNSORTED,
+//                    preferences = setOf(
+//                        SwitchPreference("morphe_aislist_submit_flyout_menu", summary = true),
+//                        TextPreference(
+//                            key = "morphe_aislist_submit_username",
+//                            inputType = InputType.TEXT
+//                        )
+//                    )
+//                ),
                 PreferenceCategory(
                     key = "morphe_hide_aislist_stats_category",
                     titleKey = "morphe_hide_stats_category_title",
